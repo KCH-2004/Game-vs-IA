fbdaa9ee0740a062f6434968b88a0ecff70cb63d
vol2rhum <kevin-chen2004@outlook.com>
2026-03-26 16:34:43 +0100

    V1 vs IA (sans élagage, sans la sélection de difficulté

--- game.py
+++ game.py
@@ -1,4 +1,6 @@
 import os
+import ia
+
 class Puissance5:
 
     def __init__(self,nl = 10, nc = 10, jeton=('O','X')):
@@ -7,7 +9,7 @@ class Puissance5:
         self.nc = nc
         self.jeton = jeton
         self.board = [[' ' for i in range(nc)] for j in range(nl)]
-        self.nouvellepartie(self.nl, self.nc, self.jeton)
+        self.nouvellepartie()
 
     def __str__(self):
         board_str = ' '.join([str((i + 1) % 10) for i in range(self.nc)]) + '\n'
@@ -28,11 +30,11 @@ class Puissance5:
         print("                                                                ")
 
 
-    def check_victoire(self, ligne, colonne, joueur):
+    def check_victoire(self, ligne, colonne, Jeton):
         """ vérifie si le dernier coup joué, qui a placé le jeton
         de joueur en (ligne,colonne) permet de gagner.
         """
-        pattern = self.jeton[joueur - 1] * 5  # si on trouve jeton qui se suivent, c'est gagné
+        pattern = Jeton * 5  # si on trouve jeton qui se suivent, c'est gagné
         if pattern in ''.join([l[colonne] for l in self.board]):  # on teste la colonne
             return True
 
@@ -63,7 +65,7 @@ class Puissance5:
 
         return False
 
-    def nouvellepartie(self, nl=10, nc=10, jeton=('O', 'X')):
+    def nouvellepartie(self):
         """
         Fonction principale qui lance la partie.
         Par défaut, on utilise nl = 10 lignes et nc = 10 colonnes
@@ -76,48 +78,83 @@ class Puissance5:
         for i in range(1,11):
             conversion_lettres_entier[chr(65 + i-1)] = i
 
-        victoire = False
         compteur = 0
+        victoire = False
+        gagnant = None
+        rep = input("Voulez vous commencer ? (répondre par oui ou non)\n")
 
-        while not victoire and compteur < nc*nl:
+        while rep.lower() != 'oui' and rep.lower() != 'non':
+            rep = input("Voulez vous commencer ? (répondre par oui ou non)\n")
+
+        if rep == "oui":
+            jetonAI,jetonJoueur = 'X', 'O'
+        else:
+            jetonAI,jetonJoueur = 'O', 'X'
+
+        while not victoire and compteur < 100:
             compteur += 1
-            # Attention, pour l'affichage, on part de joueur 1 et 2, mais pour
-            # l'accès aux jetons, c'est jeton[0] et jeton[1]
-            joueur = (compteur + 1) % 2 + 1
-            while True:
-                print(f"Joueur {joueur} ({self.jeton[joueur-1]}), Entre les coordonnées de l'endroit où tu vas jouer (A1, B2...)")
-                coord = input('Cordonnées jouée: ')
+            if jetonAI == 'X':
+                isAIturn = compteur % 2 == 0
+            else:
+                isAIturn = compteur % 2 == 1
 
-                try:
-                    if len(coord) < 2 or len(coord) > 3:
-                        raise ValueError("Format incorrect. Utilisez une lettre suivie d'un nombre (ex: B4).")
-                    lettre = coord[0].upper()
-                    partie_nombre = coord[1:]
-                    if not ('A' <= lettre <= 'J'):
-                        raise ValueError("La lettre doit être comprise entre A et J.")
-                    lettre = int(conversion_lettres_entier[(coord[0].upper())])
-                    nombre = int(partie_nombre)
-                    if not (1 <= nombre <= 10):
-                        raise ValueError("Le nombre doit être compris entre 1 et 10.")
-                    if not(self.board[nombre-1][lettre-1] == ' '):
-                        raise ValueError("Cette case a déjà été jouée.")
-                    break
-                except ValueError as erreur:
-                    print(f"Erreur : {erreur}\n")
+            if isAIturn:
+                print(f"C'est au tour de l'IA {jetonAI} de jouer...")
+                result = ia.minimax(self,1,True,jetonAI,jetonJoueur)
+                self.board[result[1][0]][result[1][1]] = jetonAI
+                os.system('cls' if os.name == 'nt' else 'clear')
+                self.show_board()
+                if compteur > 6:  # on vérifie à partir du 7eme coup
+                    victoire = self.check_victoire(result[1][0], result[1][1], jetonAI)
+                    if victoire:
+                        gagnant = jetonAI
+            else:
+                while True:
+                    print(f"Joueur (Jeton {jetonJoueur}), Entre les coordonnées de l'endroit où tu vas jouer (A1, B2...)")
+                    coord = input('Cordonnées jouée: ')
+                    try:
+                        if len(coord) < 2 or len(coord) > 3:
+                            raise ValueError("Format incorrect. Utilisez une lettre suivie d'un nombre (ex: B4).")
+                        lettre = coord[0].upper()
+                        partie_nombre = coord[1:]
+                        if not ('A' <= lettre <= 'J'):
+                            raise ValueError("La lettre doit être comprise entre A et J.")
+                        lettre = int(conversion_lettres_entier[(coord[0].upper())])
+                        nombre = int(partie_nombre)
+                        if not (1 <= nombre <= 10):
+                            raise ValueError("Le nombre doit être compris entre 1 et 10.")
+                        if not(self.board[nombre-1][lettre-1] == ' '):
+                            raise ValueError("Cette case a déjà été jouée.")
+                        break
+                    except ValueError as erreur:
+                        print(f"Erreur : {erreur}\n")
 
-            colonne = int(conversion_lettres_entier[(coord[0].upper())]) -1
-            ligne = int(coord[1:]) -1
-            self.board[ligne][colonne] = self.jeton[joueur - 1]
+                colonne = int(conversion_lettres_entier[(coord[0].upper())]) -1
+                ligne = int(coord[1:]) -1
+                self.board[ligne][colonne] = jetonJoueur
 
-            os.system('cls' if os.name == 'nt' else 'clear')
-            self.show_board()
+                os.system('cls' if os.name == 'nt' else 'clear')
+                self.show_board()
 
-            if compteur > 6:  # on vérifie à partir du 7eme coup
-                victoire = self.check_victoire(ligne, colonne, joueur)
+                if compteur > 6:  # on vérifie à partir du 7eme coup
+                    victoire = self.check_victoire(ligne, colonne, jetonJoueur)
+                    if victoire:
+                        gagnant = jetonJoueur
 
-        if victoire:
-            print(f"Le Joueur {joueur} ({self.jeton[joueur-1]}) a gagné!")
+        if gagnant == jetonJoueur:
+            print("Vous avez gagné!")
+        elif gagnant == jetonAI:
+            print("Dommage, ce n'est pas encore ça !")
         else:
             print("Match nul!")
+
+    def get_remain_moves(self):
+        res = []
+        for i in range(10):
+            for j in range(10):
+                if self.board[i][j] == ' ':
+                    res.append([i, j])
+        return res
+
 
 jeu = Puissance5()
---
+++ ia.py
@@ -0,0 +1,115 @@
+import copy
+def minimax(board, depth, isMaximizing,jetonAI, jetonJoueur,dernier_coup=None):
+    if dernier_coup is not None and board.check_victoire(dernier_coup[0],dernier_coup[1],jetonAI):
+        return (10000,None)
+
+    if dernier_coup is not None and board.check_victoire(dernier_coup[0],dernier_coup[1],jetonJoueur):
+        return (-10000,None)
+
+    if not board.get_remain_moves():
+        return (0,None)
+
+    if depth == 0:
+        return (evaluate(board,jetonAI,jetonJoueur),0)
+
+    if isMaximizing == True:
+        meilleur_score = float('-inf')
+        meilleur_coup = None
+        for coup in board.get_remain_moves():
+            bckup = copy.deepcopy(board)
+            bckup.board[coup[0]][coup[1]] = jetonAI
+            score = minimax(bckup, depth-1, not isMaximizing, jetonAI, jetonJoueur,dernier_coup=coup)
+            if score[0] > meilleur_score:
+                meilleur_score = score[0]
+                meilleur_coup = coup
+        return (meilleur_score, meilleur_coup)
+    else:
+        meilleur_score = float('+inf')
+        meilleur_coup = None
+        for coup in board.get_remain_moves():
+            bckup = copy.deepcopy(board)
+            bckup.board[coup[0]][coup[1]] = jetonJoueur
+            score = minimax(bckup, depth - 1, not isMaximizing, jetonAI, jetonJoueur,dernier_coup=coup)
+            if score[0] < meilleur_score:
+                meilleur_score = score[0]
+                meilleur_coup = coup
+        return (meilleur_score, meilleur_coup)
+
+def evaluate (board,jetonAI, jetonJoueur):
+    table_score_ouvert = {1: 1, 2: 5, 3: 50, 4: 1000}
+    table_score_semi_ouvert = {1: 0, 2: 1, 3: 10, 4: 1000}
+    score_total = 0
+
+    for ligne in board.board:
+        for i in range(6):
+            gauche_ouverte = (i > 0) and (ligne[i - 1] == ' ')
+            droite_ouverte = (i + 5 < 9) and (ligne[i + 5] == ' ')
+            fenetre = ligne[i:i + 5]
+            if jetonAI in fenetre and jetonJoueur in fenetre:
+                continue
+            elif jetonAI in fenetre and jetonJoueur not in fenetre:
+                if gauche_ouverte and droite_ouverte:
+                    score_total += table_score_ouvert[fenetre.count(jetonAI)]
+                elif gauche_ouverte or droite_ouverte:
+                    score_total += table_score_semi_ouvert[fenetre.count(jetonAI)]
+            elif jetonAI not in fenetre and jetonJoueur in fenetre:
+                if gauche_ouverte and droite_ouverte:
+                    score_total -= table_score_ouvert[fenetre.count(jetonJoueur)]
+                elif gauche_ouverte or droite_ouverte:
+                    score_total -= table_score_semi_ouvert[fenetre.count(jetonJoueur)]
+
+    for i in range(6):
+        for j in range(6):
+            fenetre = [board.board[j+k][i] for k in range(5)]
+            haut_ouverte = (j > 0) and (board.board[j-1][i] == ' ')
+            bas_ouverte = (j+5 < 9) and (board.board[j+5][i] == ' ')
+            if jetonAI in fenetre and jetonJoueur in fenetre:
+                continue
+            elif jetonAI in fenetre and jetonJoueur not in fenetre:
+                if haut_ouverte and bas_ouverte:
+                    score_total += table_score_ouvert[fenetre.count(jetonAI)]
+                elif haut_ouverte or bas_ouverte:
+                    score_total += table_score_semi_ouvert[fenetre.count(jetonAI)]
+            elif jetonAI not in fenetre and jetonJoueur in fenetre:
+                if haut_ouverte and bas_ouverte:
+                    score_total -= table_score_ouvert[fenetre.count(jetonJoueur)]
+                elif haut_ouverte or bas_ouverte:
+                    score_total -= table_score_semi_ouvert[fenetre.count(jetonJoueur)]
+
+    for i in range(4,10):
+        for j in range(6):
+            diag_haute_ouverte = (j > 0) and (i < 9) and (board.board[j-1][i+1] == ' ')
+            diag_basse_ouverte = (j+5 < 9) and (i-5 >= 0) and (board.board[j+5][i-5] == ' ')
+            fenetre = [board.board[j+k][i-k] for k in range(5)]
+            if jetonAI in fenetre and jetonJoueur in fenetre:
+                continue
+            elif jetonAI in fenetre and jetonJoueur not in fenetre:
+                if diag_haute_ouverte and diag_basse_ouverte:
+                    score_total += table_score_ouvert[fenetre.count(jetonAI)]
+                elif diag_haute_ouverte or diag_basse_ouverte:
+                    score_total += table_score_semi_ouvert[fenetre.count(jetonAI)]
+            elif jetonAI not in fenetre and jetonJoueur in fenetre:
+                if diag_haute_ouverte and diag_basse_ouverte:
+                    score_total -= table_score_ouvert[fenetre.count(jetonJoueur)]
+                elif diag_haute_ouverte or diag_basse_ouverte:
+                    score_total -= table_score_semi_ouvert[fenetre.count(jetonJoueur)]
+
+    for i in range(6):
+        for j in range(6):
+            diag_haute_ouverte = (j > 0) and (i > 0) and (board.board[j-1][i-1] == ' ')
+            diag_basse_ouverte = (j+5 < 9) and (i+5 < 9) and (board.board[j+5][i+5] == ' ')
+            fenetre = [board.board[j+k][i+k] for k in range(5)]
+            if jetonAI in fenetre and jetonJoueur in fenetre:
+                continue
+            elif jetonAI in fenetre and jetonJoueur not in fenetre:
+                if diag_haute_ouverte and diag_basse_ouverte:
+                    score_total += table_score_ouvert[fenetre.count(jetonAI)]
+                elif diag_haute_ouverte or diag_basse_ouverte:
+                    score_total += table_score_semi_ouvert[fenetre.count(jetonAI)]
+            elif jetonAI not in fenetre and jetonJoueur in fenetre:
+                if diag_haute_ouverte and diag_basse_ouverte:
+                    score_total -= table_score_ouvert[fenetre.count(jetonJoueur)]
+                elif diag_haute_ouverte or diag_basse_ouverte:
+                    score_total -= table_score_semi_ouvert[fenetre.count(jetonJoueur)]
+
+    return score_total
